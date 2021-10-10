ab0c6244c968b983dde3e07ae1384f44104285ab
1326987200@qq.com <wdy725423>
2021-10-10 13:51:09 +0800

    2222

--- wdy-service/wdy-eruka-server/src/main/java/com/wdy/ErukaApplication.java
+++ wdy-service/wdy-eruka-server/src/main/java/com/wdy/ErukaApplication.java
@@ -3,13 +3,12 @@ package com.wdy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ErukaApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ErukaApplication.class, args);
     }
-
-
 }
